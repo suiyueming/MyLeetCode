823daf31c04fad79e33ce0fb3971f8dced206dce
suiyueming <sui_yueming@163.com>
2016-03-27 22:14:50 +0800

    两两交换链表节点

--- src/com/suiyue/SwapNodesinPairs.java
+++ src/com/suiyue/SwapNodesinPairs.java
@@ -1,13 +1,23 @@
 package com.suiyue;
 
+import com.suiyue.AddTwoNumbers.ListNode;
 public class SwapNodesinPairs {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+	}
+	public ListNode swapPairs(ListNode head) {
+		//链表为空或链表只有一个节点
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
 	}
 
 }
